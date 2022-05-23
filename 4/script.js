232d34340aedbd339913dfb8cc0fa68b78c79a2d
Not-a-whale <49877969+Not-a-whale@users.noreply.github.com>
2022-05-23 22:31:58 +0200

    lab 4 completed

--- 4/script.js
+++ 4/script.js
@@ -1,6 +1,24 @@
 const dropdownButton = document.getElementById("dropdownMenuLink");
 const dropdownBlock = document.getElementById("dropdown-block");
+const dropdownButtonkMobile = document.getElementById("navbarToggler");
+const dropdownBlockMobile = document.getElementById("navbarCollapse");
+
+window.onscroll = function() {scrollFunction()};
+
 
 dropdownButton.addEventListener("click", () => {
     dropdownBlock.classList.toggle('show');
+});
+
+dropdownButtonkMobile.addEventListener("click", () => {
+    dropdownBlockMobile.classList.toggle('show');
 })
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("mainHeader").classList.add("header-scroll");
+      console.log(document.getElementById("mainHeader").style.background)
+    } else {
+      document.getElementById("mainHeader").classList.remove("header-scroll");
+    }
+}
